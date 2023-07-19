1f808c86cf856bde11cf36403e8f7f2d1725e720
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 02:56:39 -0500

    Added faker gem and seed data for testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+# Helper method to create random soccer equipment products
+def create_product(category_id)
+    Product.create(
+      name: Faker::Commerce.product_name,
+      stock: Faker::Number.between(from: 5, to: 100),
+      price: Faker::Commerce.price(range: 10.0..200.0),
+      category_id: category_id
+    )
+  end
+  
+  # Seed Categories
+  categories = [
+    { name: 'Soccer Balls', description: 'Various soccer balls for matches and training.' },
+    { name: 'Soccer Cleats', description: 'Soccer shoes for different playing surfaces.' },
+    { name: 'Goalkeeper Gloves', description: 'Gloves for soccer goalkeepers.' },
+    { name: 'Training Equipment', description: 'Training gear for soccer players.' }
+  ]
+  
+  Category.create(categories)
+  
+  # Seed Products
+  categories = Category.all
+  categories.each do |category|
+    25.times do
+      create_product(category.id)
+    end
+  end
+  
