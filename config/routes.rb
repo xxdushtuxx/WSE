8f373a850df1476498220555afae3c6d2ad491a3
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 00:53:07 -0500

    Added AdminDashboard controller and new routes for it.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'admin_dashboard/index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
