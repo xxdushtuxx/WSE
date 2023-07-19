8cfcea8fb73f56969fe424ab600f8d89fd4f0e16
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 01:05:02 -0500

    Added helper methods in Application controller to manage admin sessions.

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,4 +1,6 @@
 class AdminDashboardController < ApplicationController
+  before_action :require_admin
+
   def index
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+    helper_method :current_admin, :logged_in?
+  
+    def current_admin
+      @current_admin ||= Admin.find_by(id: session[:admin_id])
+    end
+  
+    def logged_in?
+      !!current_admin
+    end
+  
+    def require_admin
+      redirect_to login_path, alert: 'You must be logged in as an admin to access this page' unless logged_in?
+    end
+  end
+  
