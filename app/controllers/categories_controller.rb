235f4f74cc5436152745ff401c7cac998247fcda
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 03:10:49 -0500

    Generated Products and Categories controller and implemented CRUD.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,35 @@
+class CategoriesController < ApplicationController
+  def index
+    @category = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: 'Category was successfully deleted.'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
+end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,34 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: 'Product was successfully deleted.'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :stock, :price, :category_id)
+  end
+end
