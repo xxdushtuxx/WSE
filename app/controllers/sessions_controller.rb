3c3e974535025101040d3e88e1c412bff54bdfb2
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 01:43:20 -0500

    Resolved the logout issue of the admin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     end
   
     def create
-      user = Admin.find_by(username: params[:session][:username])
+        user = Admin.find_by(username: session_params[:username])
       if user && user.authenticate(params[:session][:password])
         session[:admin_id] = user.id
         redirect_to admin_dashboard_index_path, notice: 'Logged in successfully!'
@@ -17,5 +17,10 @@ class SessionsController < ApplicationController
       session[:admin_id] = nil
       redirect_to login_path, notice: 'Logged out successfully!'
     end
+
+    def session_params
+        params.require(:session).permit(:username, :password)
+    end
+      
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'admin_dashboard/index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 end
