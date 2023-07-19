ac106bb8dc8d051d26670e490f4f6b3378f703e3
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 02:39:54 -0500

    Generated StaticPages controller to manage the static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'  
+  get 'about', to: 'static_pages#about'
+  get 'contact', to: 'static_pages#contact'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   get 'admin_dashboard/index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
