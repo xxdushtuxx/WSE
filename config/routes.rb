6625101ce5e1ea8db21cec995d4e93eea264b1d8
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-19 15:36:42 -0500

    Resolved issue with routes for products list display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/destroy'
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/destroy'
   root 'static_pages#home'  
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
@@ -18,8 +8,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
 
-  resources :categories do
-    resources :products
-  end
-  get '/products', to: 'products#index_all', as: 'all_products'
+  resources :categories 
+  resources :products
+  #get '/products', to: 'products#index_all', as: 'all_products'
 end
