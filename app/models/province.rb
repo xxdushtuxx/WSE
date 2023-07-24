1d92d6beba6850c384691f3f279ae3f3e7a07c2b
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-24 10:05:46 -0500

    Added validations and defined ransackable attributes in Province model.

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,2 +1,12 @@
-class Province < ApplicationRecord
-end
+# app/models/province.rb
+
+class Province < ApplicationRecord  
+    # Validations
+    validates :name, presence: true, uniqueness: true
+    validates :pst, :hst, :gst, :applicable, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  
+    def self.ransackable_attributes(auth_object = nil)
+      ["id", "name", "pst", "hst", "gst", "applicable"]
+    end
+  end
+  
