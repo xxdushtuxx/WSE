1233fc7bec4b066bb1228616a474f388fc458d03
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-24 10:40:45 -0500

    Updated product model validation to fix a bug.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,10 @@ class Product < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :sale_status, inclusion: { in: ['yes', 'no'] }
   validates :category, presence: true
-  
+
   def self.ransackable_attributes(auth_object = nil)
     ["category_id", "created_at", "description", "id", "name", "price", "sale_status", "stock", "updated_at"]
   end
