0e9cbc0a92309349a20a8651a1e784650f1e792b
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-24 10:34:18 -0500

    Added validations to customer model and edited app/admin/customers.rb

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,18 @@
 ActiveAdmin.register Customer do
+  permit_params :province_id, :first_name, :last_name, :email, :phone, :address, :city, :postal_code
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :province_id, :first_name, :last_name, :email, :phone, :address, :city, :postal_code
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:province_id, :first_name, :last_name, :email, :phone, :address, :city, :postal_code]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  form do |f|
+    f.semantic_errors # This line will display error messages on top of the form if there are any validation errors.
+    f.inputs 'Customer Details' do
+      f.input :province_id, as: :select, collection: Province.all, include_blank: false # Assuming you have a Province model with a 'name' attribute.
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :phone
+      f.input :address
+      f.input :city
+      f.input :postal_code
+    end
+    f.actions
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,14 @@
 class Customer < ApplicationRecord
   belongs_to :province
+
+  validates :first_name, :last_name, :email, :phone, :address, :city, :postal_code, presence: true
+  validates :first_name, :last_name, length: { maximum: 50 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, format: { with: /\A[\d\+\-\(\)]+\z/, message: "only allows digits, +, -, (, and )" }
+  validates :email, uniqueness: true
+  validates :postal_code, numericality: { only_integer: true }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "city", "created_at", "email", "first_name", "id", "last_name", "phone", "postal_code", "province_id", "updated_at"]
+  end
 end
