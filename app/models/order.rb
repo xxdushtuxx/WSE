ec8e848643724e87ad8d4d51c8ae882fecc8f043
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-25 08:45:49 -0500

    Added validations and defined associations in order model.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :products, through: :order_items
+
+  validates :customer_id, presence: true
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
 end
