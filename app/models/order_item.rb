4069c13fb3b01688aedead0aadc32cb9003124bf
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-25 08:53:50 -0500

    Added validations to OrderItem and associations to Product model.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :order_id, :product_id, :quantity, :cost, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :cost, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :order_items
+  has_many :orders, through: :order_items
   belongs_to :category
 
   validates :name, presence: true, length: { maximum: 255 }
