4a57c3917b8107587282efd645e3aecf1d6bad1e
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-07-25 23:12:36 -0500

    Removing product from cart is enabled.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,6 +12,12 @@ class CartsController < ApplicationController
         redirect_to products_path
     end
 
+    def remove_from_cart
+        id = params[:id].to_i  
+        session[:cart].delete(id)
+        redirect_to products_path
+    end
+
     private
 
     def initialize_cart_session
