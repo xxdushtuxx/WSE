bb3614e81e482cfe8f10825d0e9df41af57e891b
xxdushtuxx <xxdushtuxx786@gmail.com>
2023-08-02 00:55:38 -0500

    New column added to orders table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_133702) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_055356) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_133702) do
     t.decimal "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "new"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
